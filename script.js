c696c1122384ba8187488f38da24eadcc59d6d0e
yacobuswahyu <62925525+yacobuswahyu@users.noreply.github.com>
2021-11-03 12:09:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,19 @@ function hitung(pilihan)
 {
     var angka1=document.getElementById('input1').value;
     var angka2=document.getElementById('input2').value;
-    var angka3=document.getElementById('input3').value;
     angka1 = parseInt(angka1)
     angka2 = parseInt(angka2)
-    angka3 = parseInt(angka3)
     if (pilihan=='+'){
-        document.getElementById('hasil').value = angka1 + angka2 + angka3;
+        document.getElementById('hasil').value = angka1 + angka2;
     }
     else if(pilihan == '-'){
-        document.getElementById('hasil').value = angka1 - angka2 - angka3;
+        document.getElementById('hasil').value = angka1 - angka2;
     }
     else if(pilihan == 'x'){
-        document.getElementById('hasil').value = angka1 * angka2 * angka3;
+        document.getElementById('hasil').value = angka1 * angka2;
     }
     else {
-        document.getElementById('hasil').value = angka1 / angka2 / angka3;
+        document.getElementById('hasil').value = angka1 / angka2;
     }
     document.getElementById('hasil');
 }
